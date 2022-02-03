7cad1c514e559f103fdb3e735271d7ad796ffb33
HASSAN1A <okothhassanjuma@gmail.com>
2022-02-03 22:28:40 +0300

    add data loader

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,30 +1,39 @@
 """
-
     Helper functions for data loading and manipulation.
-
     Author: Explore Data Science Academy.
-
 """
 # Data handling dependencies
 import pandas as pd
 import numpy as np
+import streamlit as st
 
 def load_movie_titles(path_to_movies):
     """Load movie titles from database records.
-
     Parameters
     ----------
     path_to_movies : str
         Relative or absolute path to movie database stored
         in .csv format.
-
     Returns
     -------
     list[str]
         Movie titles.
-
     """
     df = pd.read_csv(path_to_movies)
     df = df.dropna()
-    movie_list = df['title'].to_list()
+    movie_list = list(df['title'])
     return movie_list
+
+def load_dataframe(path_to_csv, index):
+    """Load train data from database records.
+    Parameters
+    ----------
+    path_to_train : str
+        Relative or absolute path to interactions database stored
+        in .csv format.
+    Returns
+    -------
+    df : DataFrame
+        User Interactions dataframe.
+    """
+    return pd.read_csv(path_to_csv,index_col=index)
