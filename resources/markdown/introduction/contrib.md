d60c39b3e6cedf8d1ca88b963b7ee7aa0d490440
HASSAN1A <okothhassanjuma@gmail.com>
2022-02-04 17:30:22 +0300

    add markdown files

---
+++ resources/markdown/introduction/contrib.md
@@ -0,0 +1,13 @@
+### Page Descriptions
+
+
+**Introduction** - Read about the project and its amazing contributors.  
+
+**Exploratory Data Analysis** - Explore and draw insights from movie data.  
+
+**Recommender System** - Choose a recommender model to predict some movies that you may like.  
+
+**Solution Overview** - Read a short discussion of our best performing model.  
+
+
+
--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,6 +1,9 @@
 """
+
     Helper functions for data loading and manipulation.
+
     Author: Explore Data Science Academy.
+
 """
 # Data handling dependencies
 import pandas as pd
@@ -9,15 +12,18 @@ import streamlit as st
 
 def load_movie_titles(path_to_movies):
     """Load movie titles from database records.
+
     Parameters
     ----------
     path_to_movies : str
         Relative or absolute path to movie database stored
         in .csv format.
+
     Returns
     -------
     list[str]
         Movie titles.
+
     """
     df = pd.read_csv(path_to_movies)
     df = df.dropna()
@@ -26,14 +32,17 @@ def load_movie_titles(path_to_movies):
 
 def load_dataframe(path_to_csv, index):
     """Load train data from database records.
+
     Parameters
     ----------
     path_to_train : str
         Relative or absolute path to interactions database stored
         in .csv format.
+
     Returns
     -------
     df : DataFrame
         User Interactions dataframe.
+
     """
     return pd.read_csv(path_to_csv,index_col=index)
